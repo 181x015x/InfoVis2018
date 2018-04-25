39faeb9a0e186dceb42bdcd207ff4b9c42a595d2
181x015x <38451502+181x015x@users.noreply.github.com>
2018-04-25 13:40:47 +0900

    Update main01.js

--- 06/main01.js
+++ 06/main01.js
@@ -25,8 +25,8 @@ function main()
     var material = new THREE.ShaderMaterial({
         vertexColors: THREE.VertexColors,
         vertexShader: document.getElementById('gouraud.vert').text,
-        fragmentShader: document.getElenebtById('gouraud.frag').text,
-        uniforns: {
+        fragmentShader: document.getElementById('gouraud.frag').text,
+        uniforms: {
             light_position: {type: 'v3', value: light.position}
         }
     }};
